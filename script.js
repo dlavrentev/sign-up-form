654998d3d7597d7080a91ab7e1f5496800cded47
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-15 15:15:33 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+
+let isValid = false;
+
+function validateForm() {
+    // Using Contraint API
+    isValid = form.checkValidity();
+    console.log(isValid);
+}
+
+
+function processFormData(e) {
+     e.preventDefault();
+     // Validate Form
+     validateForm();
+}
+
+// Event Listener 
+form.addEventListener('submit' , processFormData);
